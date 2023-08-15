cd29360f5708285492671e4af8c304c889ba4b1c
Nguyễn Tấn Lộc <tanlocnguyen1902@gmail.com>
2023-08-15 11:29:21 +0700

    Talo-update dc motor

--- src/dcMotor.cpp
+++ src/dcMotor.cpp
@@ -1,71 +1,115 @@
 // C++ code
 //
-// Khai báo chân kết nối đến module điều khiển động cơ DC (L298N)
+
 #include "dcMotor.h"
 
 // Hàm khởi tạo cấu hình ban đầu
-void setupDCMotor() {
-  // Khai báo các chân là OUTPUT
-  pinMode(enablePin, OUTPUT);
+void setupDCMotor()
+{
+  // Khai báo các chân của 2 Motor đóng mở khay là OUTPUT
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
 
-  pinMode(2,OUTPUT);
+  // Khai báo chân đèn tín hiệu
+  pinMode(2, OUTPUT);
+
   // Tắt động cơ ban đầu
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
 }
 
 // Hàm mở khay thức ăn
-void openFoodTray() {
+void openFoodTray()
+{
   // Chiều quay động cơ để mở khay
   digitalWrite(motorPin1, HIGH);
   digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, HIGH);
 
   // Cấp tín hiệu PWM để điều chỉnh tốc độ động cơ
-  analogWrite(enablePin, 10); // Giá trị PWM có thể điều chỉnh từ 0 (tắt) đến 255 (tối đa tốc độ)
-  delay(4000); // Dừng mở trong 2 giây
+  delay(4000); // Dừng mở trong 4 giây
 
   // Tắt động cơ sau khi mở khay
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
 }
 
 // Hàm đóng khay thức ăn
-void closeFoodTray() {
+void closeFoodTray()
+{
   // Chiều quay động cơ để đóng khay
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, HIGH);
+  digitalWrite(motorPin3, HIGH);
+  digitalWrite(motorPin4, LOW);
 
   // Cấp tín hiệu PWM để điều chỉnh tốc độ động cơ
-  analogWrite(enablePin, 10); // Giá trị PWM có thể điều chỉnh từ 0 (tắt) đến 255 (tối đa tốc độ)
-  delay(4000); // Dừng đóng trong 1.5 giây
+  delay(4000); // Dừng đóng trong 4 giây
 
   // Tắt động cơ sau khi đóng khay
   digitalWrite(motorPin1, LOW);
   digitalWrite(motorPin2, LOW);
+  digitalWrite(motorPin3, LOW);
+  digitalWrite(motorPin4, LOW);
 }
 
+// Hàm mở phễu
+void openFunnel()
+{
+  // Chiều quay động cơ để mở khay
+  digitalWrite(funnelPin1, HIGH);
+  digitalWrite(funnelPin2, LOW);
+
+  // Cấp tín hiệu PWM để điều chỉnh tốc độ động cơ
+  delay(4000); // Dừng mở trong 4 giây
+}
+
+// Hàm đóng phễu
+void closeFunnel()
+{
+  // Chiều quay động cơ để đóng khay
+  digitalWrite(funnelPin1, LOW);
+  digitalWrite(funnelPin2, HIGH);
+
+  // Cấp tín hiệu PWM để điều chỉnh tốc độ động cơ
+  delay(4000); // Dừng đóng trong 4 giây
+}
+
+// Hàm mở đèn tín hiệu
 void turnOnLed()
 {
-  digitalWrite(2,HIGH);
+  digitalWrite(2, HIGH);
   delay(1000);
-  digitalWrite(2,LOW);
+  digitalWrite(2, LOW);
 }
 // Hàm chính
-void loopDCMotor() {
+void loopDCMotor()
+{
+
+  // Gọi hàm để mở phễu thức ăn
+  openFunnel();
+
+  // Gọi hàm để đóng phễu thức ăn
+  closeFunnel();
+
   // Gọi hàm mở khay thức ăn
   openFoodTray();
-	
+
   turnOnLed();
-  
+
   delay(5000); // Đợi 5 giây trước khi đóng khay thức ăn
 
   // Gọi hàm đóng khay thức ăn
   closeFoodTray();
-  
-  turnOnLed();
-  
-  delay(5000); // Đợi 5 giây trước khi mở khay thức ăn lại
 
+  turnOnLed();
+
+  delay(5000); // Đợi 5 giây trước khi mở khay thức ăn lại
 }
--- src/dcMotor.h
+++ src/dcMotor.h
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 
 // Khai báo chân kết nối đến module điều khiển động cơ DC (L298N)
-const int enablePin = 9;  // Chân enable để điều khiển tốc độ động cơ
-const int motorPin1 = 13; // Chân IN1 điều khiển chiều quay động cơ
-const int motorPin2 = 12; // Chân IN2 điều khiển chiều quay động cơ
+const int motorPin1 = 13; // Chân IN1 điều khiển chiều quay động cơ khay
+const int motorPin2 = 12; // Chân IN2 điều khiển chiều quay động cơ khay
+const int motorPin3 = 10; // Chân IN3 điều khiển chiều quay động cơ khay
+const int motorPin4 = 8;  // Chân IN4 điều khiển chiều quay động cơ khay
+const int funnelPin1 = 6; // Chân IN5 điều khiển chiều quay động cơ phễu
+const int funnelPin2 = 4; // // Chân IN4 điều khiển chiều quay động cơ phễu
 
 void setupDCMotor();
 void loopDCMotor();
 void openFoodTray();
 void closeFoodTray();
+void openFunnel();
+void closeFunnel();
 void turnOnLed();
